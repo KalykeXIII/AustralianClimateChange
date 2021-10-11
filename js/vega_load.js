5dcf2f656160e6a6b58961246de0ddef355ef1a4
KalykeXIII <kyleherbertson4@gmail.com>
2021-10-12 02:47:10 +1100

    Topojson acquisition and initial figure setup

--- js/vega_load.js
+++ js/vega_load.js
@@ -1,4 +1,4 @@
-var vg_1 = "";
+var vg_1 = "emissions.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
