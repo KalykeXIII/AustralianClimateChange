c768cdcf902bde99cf98fc868bc3027ff7bc6952
KalykeXIII <kyleherbertson4@gmail.com>
2021-10-13 01:32:11 +1100

    Fixing of choropleths and section 1 formatting of HTML site

--- js/vega_load.js
+++ js/vega_load.js
@@ -1,5 +1,9 @@
-var vg_1 = "emissionsDual.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+var raw = "emissionsRaw.vg.json";
+vegaEmbed("#rawEmissions", raw).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var capita = "emissionsCapita.vg.json";
+vegaEmbed("#capitaEmissions", capita).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
