a7437b86bb51ba0ef8b5c9f1267b429a4a7792a3
KalykeXIII <kyleherbertson4@gmail.com>
2021-10-17 21:28:55 +1100

    CO2 change dataset

--- js/vega_load.js
+++ js/vega_load.js
@@ -3,7 +3,7 @@ vegaEmbed("#rawEmissions", raw).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var capita = "emissionsCapita.vg.json";
+var capita = "emissionsCapitaCombined.vg.json";
 vegaEmbed("#capitaEmissions", capita).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
